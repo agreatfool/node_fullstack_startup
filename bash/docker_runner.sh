223311bbd75c612e50b219f5044ebcd26447ee32
Jonathan Dai <nightghost5078@gmail.com>
2019-11-11 13:34:49 +0800

    Update.

---
+++ bash/docker_runner.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+FULLPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd ${FULLPATH}/..
+
+VERSION="0.0.1"
+
+# prepare docker context (leave it empty)
+rm -rf ./docker
+mkdir -p ./docker/context # since docker COPY command can only copy files & sub dirs of a source dir rather than the source dir itself, so ./docker/context is the actual context dir
+
+# build image
+docker build \
+    --no-cache \
+    --tag fullstack_runner:${VERSION} \
+    --file ./Dockerfile_runner \
+    ./docker
+
+# remove images without tags
+docker rmi $(docker images | awk '/^<none>/ {print $3}')
+
+# remove tmp file
+rm -rf ./docker
