c4c72a0036be4fb9c004203cb2478351753704f6
Jonathan Dai <nightghost5078@gmail.com>
2019-11-13 10:06:35 +0800

    Update.

--- common/build/common/src/database/database.d.ts
+++
@@ -1,4 +0,0 @@
-import { ConnectionOptions } from "typeorm";
-export declare class Database {
-    static getConnectionOptions(): ConnectionOptions;
-}
--- common/build/server/src/logger/logger.d.ts
+++
@@ -1,5 +0,0 @@
-import { Logger as CommonLogger } from "common";
-export declare class Logger {
-    static get(): CommonLogger.Logger;
-    private static instance;
-}
