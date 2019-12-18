2060a3947697b5b1738b52b1c1cef8b7a8f927d5
Jonathan Dai <nightghost5078@gmail.com>
2019-12-18 14:29:45 +0800

    Update.

--- bash/registry.sh
+++ bash/registry.sh
@@ -4,6 +4,8 @@ REGISTRY="127.0.0.1:15000"
 REGISTRY_USER="test"
 REGISTRY_PWD="abc123_"
 
+# /# /bin/registry garbage-collect /etc/docker/registry/config.yml
+
 function catalog() {
     curl -u ${REGISTRY_USER}:${REGISTRY_PWD} http://${REGISTRY}/v2/_catalog
 }
@@ -12,13 +14,22 @@ function tags() {
     curl -u ${REGISTRY_USER}:${REGISTRY_PWD} http://${REGISTRY}/v2/$1/tags/list
 }
 
-function usage() {
-    echo "Usage: registry.sh catalog|tags"
+function digest() {
+    curl -u ${REGISTRY_USER}:${REGISTRY_PWD} http://${REGISTRY}/v2/$1/manifests/$2 \
+        --header "Accept: application/vnd.docker.distribution.manifest.v2+json" | jq '.config|.digest'
 }
 
-if [[ $1 != "catalog" ]] && [[ $1 != "tags" ]]; then
+function delete() {
+    curl -u ${REGISTRY_USER}:${REGISTRY_PWD} -I -X DELETE "http://${REGISTRY}/v2/$1/manifests/$2"
+}
+
+function usage() {
+    echo "Usage: registry.sh catalog | tags \$repo_name | digest \$repo_name \$tag | delete \$repo_name \$digest"
+}
+
+if [[ $1 != "catalog" ]] && [[ $1 != "tags" ]] && [[ $1 != "digest" ]] && [[ $1 != "delete" ]]; then
     usage
     exit 0
 fi
 
-eval $1 $2
+eval $1 $2 $3
