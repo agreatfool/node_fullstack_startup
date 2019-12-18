f074c532ce1bcc00dde6768592e61460f5c3912f
Jonathan Dai <nightghost5078@gmail.com>
2019-12-18 14:38:46 +0800

    Update.

--- gateway/bash/docker.sh
+++ gateway/bash/docker.sh
@@ -1,32 +1,39 @@
 #!/usr/bin/env bash
 
 FULLPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd ${FULLPATH}/../..
 
-VERSION=`cat ./gateway/package.json | jq -r '.version'`
-COMMON_VERSION=`cat ./common/package.json | jq -r '.version'`
+cd ${FULLPATH}/.. # gateway root
+
+# prepare private registry secret
+if [[ ! -d "~/.docker" ]];then
+    mkdir ~/.docker
+    cp ./dockerconfigjson ~/.docker/config.json
+fi
+
+VERSION=`cat ./package.json | jq -r '.version'`
+COMMON_VERSION="0.0.28" # since drone env has current repo only, it has to be hard-coded
 
 REGISTRY="127.0.0.1:15000"
 ORIGIN_TAG=fullstack_gateway:${VERSION}
 REGISTRY_TAG=${REGISTRY}/fullstack/gateway:${VERSION}
 
 # prepare docker context
-rm -rf ./docker
-mkdir -p ./docker/context # since docker COPY command can only copy files & sub dirs of a source dir rather than the source dir itself, so ./docker/context is the actual context dir
-mkdir -p ./docker/context/logs # business logs
-mkdir -p ./docker/context/pm2  # pm2 logs
+rm -rf /tmp/docker
+mkdir -p /tmp/docker/context/gateway # since docker COPY command can only copy files & sub dirs of a source dir rather than the source dir itself, so ./docker/context is the actual context dir
+mkdir -p /tmp/docker/context/logs # business logs
+mkdir -p /tmp/docker/context/pm2  # pm2 logs
 
 rsync -av \
-    gateway \
-    ./docker/context \
+    ./* \
+    /tmp/docker/context/gateway \
     --exclude build \
     --exclude node_modules \
     --exclude .gitignore \
     --exclude Dockerfile \
     --exclude README.md
 
-cp ./fullstack.container.yml ./docker/context
-mv ./docker/context/fullstack.container.yml ./docker/context/fullstack.yml
+cp ./fullstack.container.yml /tmp/docker/context/gateway
+mv /tmp/docker/context/gateway/fullstack.container.yml /tmp/docker/context/gateway/fullstack.yml
 
 # build image
 docker build \
@@ -34,14 +41,14 @@ docker build \
     --build-arg COMMON_VERSION=${COMMON_VERSION} \
     --build-arg REGISTRY=${REGISTRY} \
     --tag ${ORIGIN_TAG} \
-    --file ./gateway/Dockerfile \
-    ./docker
+    --file ./Dockerfile \
+    /tmp/docker
 
 # remove images without tags
 docker rmi $(docker images | awk '/^<none>/ {print $3}')
 
 # remove tmp file
-rm -rf ./docker
+rm -rf /tmp/docker
 
 # push image
 docker tag ${ORIGIN_TAG} ${REGISTRY_TAG}
